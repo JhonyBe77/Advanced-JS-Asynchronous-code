ad77a73fa48255986dc35133b115f16a423d7a86
JhonyBe77 <jjruiperezf@gmail.com>
2024-10-14 09:12:08 +0200

    Último ejercicio

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 //RESUELVE TUS EJERCICIOS AQUI
 
 //1
+/* Declara una funcion getAllBreeds
+que devuelva un array de strings con todas las razas de perro. */
 
 function getAllBreeds(){
-    let message = fetch('https://dog.ceo/api/breeds/list/all')
+    let breeds = fetch('https://dog.ceo/api/breeds/list/all')
     .then(res=> res.json())
     .then(data=> Object.keys(data.message))
     .catch(err => console.log("Error: "+ err.message));
-    return message;
+    return breeds;
 }
-getAllBreeds()
 
 //2
+/* Declara una función getRandomDog
+que obtenga una imagen random de una raza. */
 
 function getRandomDog(){
     let imagen = fetch(`https://dog.ceo/api/breeds/image/random`)
@@ -21,9 +24,10 @@ function getRandomDog(){
 
     return imagen;
 }
-getRandomDog();
 
 //3
+/* Declara una función getAllImagesByBreed
+que obtenga todas las imágenes de la raza komondor. */
 
 function getAllImagesByBreed(){
     let imagenRaza = fetch(`https://dog.ceo/api/breed/komondor/images`)
@@ -33,9 +37,10 @@ function getAllImagesByBreed(){
 
     return imagenRaza;
 }
-getAllImagesByBreed()
 
 //4
+/* Declara una funcion getAllImagesByBreed2(breed)
+que devuelva las imágenes de la raza pasada por el argumento */
 
 function getAllImagesByBreed2(breed){
     let imagenRaza = fetch(`https://dog.ceo/api/breed/${breed}/images`)
@@ -45,70 +50,97 @@ function getAllImagesByBreed2(breed){
 
     return imagenRaza;
 }
-getAllImagesByBreed2('hound')
 
 //5
+
+/*
+ Declarara una función getGitHubUserProfile(username)
+ que obtenga el perfil de usuario de github
+ a partir de su nombre de usuario. (https://api.github.com/users/{username}). */
 
 function getGitHubUserProfile(username){
     let perfil = fetch(`https://api.github.com/users/${username}`)
     .then(res => res.json())
-    .then(data => data.name)
+    .then(data => data)
     .catch(err => console.log("Error: "+ err.message));
 
-
     return perfil;
 }
-console.log(getGitHubUserProfile('alenriquez96'))
 
 // 6
+/* Declara una función printGithubUserProfile(username)
+que reciba como argumento el nombre de un usuario (username),
+retorne {img, name} y pinte la foto y el nombre en el DOM. */
 
-function printGithubUserProfile(username){
-    let perfil = fetch(`https://api.github.com/users/${username}`)
-    .then(res => res.json())
-    .then(data => {
-        let name = data.name;
-        let img = document.createElement('img');
-        img.setAttribute('src', data.avatar_url);
-        img.setAttribute('alt', data.name);
-        let p = document.createElement('p');
-        p.textContent = data.name;
-        document.body.appendChild(img);
-        document.body.appendChild(p);
-        return {img,name};
+function printGithubUserProfile(username) {
+    const result = getGitHubUserProfile(username)
+        .then(data => {
+            let name = data.name;
+            let img = data.avatar_url
+            // Pintar en el DOM
+            let imgDOM = document.createElement('img');
+            imgDOM.setAttribute('src', data.avatar_url);
+            imgDOM.setAttribute('alt', data.name);
+
+            let p = document.createElement('p');
+            p.textContent = data.name;
+
+            document.body.appendChild(imgDOM);
+            document.body.appendChild(p);
+            // Devolver lo que nos pide
+            return { img, name };
         })
-    return perfil;
+        .catch(err => console.log("MENSAJE DE ERROR: " + err.message));
+    return result
 }
-printGitHubUserProfile('alenriquez96')
-
 // 7 
 
-function getAndPrintGithubUserProfile(username){
-    let perfil = fetch(`https://api.github.com/users/${username}`)
-    .then(res => res.json())
-    .then(data => {
-        let section = document.createElement('section');
-        let imagen = document.createElement('img');
-        img.setAttribute('src', data.avatar_url);
-        img.setAttribute('alt', 'imagen de usuario');
-        let h1 = document.createElement('h1');
-        h1.textContent = data.name;
-        let p = document.createElement('p');
-        p.textContent = 'Public repos: ($(data.public_repos)'
-        document.body.appendChild(section);
-        section.appendChild(img);
-        section.appendChild(h1);
-        section.appendChild(p);
+/* Crea una función getAndPrintGitHubUserProfile(username)
+que contenga una petición a la API para obtener información
+de ese usuario y devuelva un string que represente una tarjeta HTML
+como en el ejemplo, la estructura debe ser exactamente la misma:
 
-        return `<section>
+<section>
+    <img src="url de imagen" alt="imagen de usuario">
+    <h1>Nombre de usuario</h1>
+    <p>Public repos: (número de repos)</p>
+</section> */
+
+function getAndPrintGitHubUserProfile(username) {
+    let perfil = fetch(`https://api.github.com/users/${username}`)
+        .then(res => res.json())
+        .then(data => {
+            let section = document.createElement('section');
+            let img = document.createElement('img');
+            img.setAttribute('src', data.avatar_url);
+            img.setAttribute('alt', "imagen de usuario");
+            let h1 = document.createElement('h1');
+            h1.textContent = data.name;
+            let p = document.createElement('p');
+            p.textContent = `Public repos: (${data.public_repos})`
+            document.body.appendChild(section);
+            section.appendChild(img);
+            section.appendChild(h1);
+            section.appendChild(p);
+            return `
+                <section>
                     <img src="${data.avatar_url}" alt="${data.name}">
                     <h1>${data.name}</h1>
                     <p>Public repos: ${data.public_repos}</p>
-                </section>`
+                </section>
+            `
         })
     return perfil;
 }
 
 //8
+
+/* Manipulación del DOM: Crea un input de tipo texto,
+y un botón buscar. El usuario escribirá en el input el nombre de usuario
+de GitHub que quiera buscar. Después llamaremos
+a la función getAndPrintGitHubUserProfile(username)
+que se ejecute cuando se pulse el botón buscar.(Esto no se testea). */
+
 // Cargar el DOM antes para hacer el último ejercicio
 document.addEventListener('DOMContentLoaded', function () {
 let btnBuscar = document.querySelector("#buscar");
@@ -123,3 +155,34 @@ let btnBuscar = document.querySelector("#buscar");
     })
 
 });
+
+//9
+
+/* Dada una lista de usuarios de github guardada en una array,
+crea una funcion fetchGithubUsers(userNames) que utilice 'https://api.github.com/users/${name}' para obtener el nombre de cada usuario.
+Objetivo: Usar Promise.all()
+Recordatorio: Una llamada a fetch() devuelve un objeto promesa.
+Pregunta. ¿cuántas promesas tendremos?
+Hasta que no se resuelvan todas las promesas desencadenadas por cada fetch(), no se cargarán los datos.
+
+Pasos:
+
+Mapear el array y hacer un fetch() para cada usuario.
+Esto nos de vuelve un array lleno de promesas.
+Con Promise.all() harás que se tenga que resolver
+todo el proceso de peticiones a GitHub a la vez.
+Cuando Promise.all() haya terminado: Consigue que se imprima
+por consola la url del repositorio de cada usuario.
+Consigue que se imprima por consola el nombre de cada usuario. */
+
+function fetchGithubUsers(userNames) {
+    Promise.all(
+        userNames.map(nombre => fetch(`https://api.github.com/users/${nombre}`).then(res => res.json()))
+    ).then(data => {
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].name)
+        }
+    })
+}
+
+    fetchGithubUsers(['octocat', 'alenriquez96', 'alejandroereyesb']).then(users => console.log(user))
